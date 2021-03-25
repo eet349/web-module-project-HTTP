7cdc5aa36b5d0fef52f9d6b65c03d2b6dc9c7f87
eet349 <51262590+eet349@users.noreply.github.com>
2021-03-25 18:06:47 -0500

    Adds Prod env port.

--- server.js
+++ server.js
@@ -106,7 +106,7 @@ app.delete("/api/movies/:id", (req, res) => {
 app.get("/", function(req, res) {
   res.send("App is working 👍");
 });
-
+const PORT = process.env.PORT || 5000
 app.listen(5000, () => {
   console.log("Server listening on port 5000");
 });
